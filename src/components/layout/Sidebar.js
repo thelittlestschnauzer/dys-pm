664ec78832602d6439f59b671cb2e76eae45341b
Dana ONeil <doneil1580@gmail.com>
2020-02-08 23:00:28 -0500

    adding styles for the navigation and the sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -5,19 +5,19 @@ import { FaChevronCircleDown, FaInbox, FaClipboardList, FaCalendarWeek} from 're
 export const Sidebar = () => (
   <div className="sidebar" data-testid="sidebar">
     <ul className="sidebar__generic">
-      <li>
+      <li data-testid="inbox" className="inbox">
         <span>
           <FaInbox />
         </span>
         <span>Inbox</span>
       </li>
-      <li>
+      <li data-testid="today" className="today">
         <span>
           <FaClipboardList />
         </span>
         <span>Today</span>
       </li>
-      <li>
+      <li data-testid="next_7" className="next_7">
         <span>
           <FaCalendarWeek />
         </span>
