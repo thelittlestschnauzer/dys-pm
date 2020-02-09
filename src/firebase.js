035f7c38095385aea50d946df570c340c69fbd43
Dana ONeil <doneil1580@gmail.com>
2020-02-08 19:12:48 -0500

    adding the tasks and beginning of styling

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messageSenderId: '',
-  apiId: '',
+  apiKey: "AIzaSyBmg7EWC0W7I_ZjxM_ue-yRD1YtD00SreU",
+  authDomain: "dys-pm.firebaseapp.com",
+  databaseURL: "https://dys-pm.firebaseio.com",
+  projectId: "dys-pm",
+  storageBucket: "dys-pm.appspot.com",
+  messagingSenderId: "465248405222",
+  appId: "1:465248405222:web:6b285001c3bc8c7c1fdd27"
 });
 
 export { firebaseConfig as firebase };
