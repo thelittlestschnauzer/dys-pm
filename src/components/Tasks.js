bb180f8a2c5367b5bc4c2d936117f2e3db90cef7
Dana ONeil <doneil1580@gmail.com>
2020-02-09 21:14:38 -0500

    adding and deleting projects

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Checkbox } from './Checkbox';
 import { useTasks } from '../hooks';
+import { collatedTasks } from '../constants';
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
+import { useSelectedProjectValue, useProjectsValue } from '../context';
+
+
 
 export const Tasks = () => {
-  const { tasks } = useTasks('1');
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  const { tasks } = useTasks(selectedProject);
 
-  console.log(tasks);
 
-  const projectName = '';
+  let projectName = '';
+
+  if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+    projectName = getTitle(projects, selectedProject).name;
+    console.log('projectName 1: ', projectName);
+  }
+
+  if (collatedTasksExist(selectedProject) && selectedProject) {
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+    console.log('projectName 2: ', projectName);
+  }
+
+  useEffect(() => {
+    document.title = `${projectName}: Todo`;
+  });
+
+console.log('tasks', tasks);
 
   return (
     <div className="tasks" data-testid="tasks">
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaChevronCircleDown, FaInbox, FaClipboardList, FaCalendarWeek} from 'react-icons/fa';
+import { useSelectedProjectValue } from '../../context';
+import { Projects } from '../Projects';
+import { AddProject } from '../AddProject';
 
 
-export const Sidebar = () => (
+export const Sidebar = () => {
+  const { setSelectedProject } = useSelectedProjectValue();
+  const [active, setActive] = useState('inbox');
+  const [showProjects, setShowProjects] = useState(true);
+
+
+  return (
   <div className="sidebar" data-testid="sidebar">
     <ul className="sidebar__generic">
-      <li data-testid="inbox" className="inbox">
-        <span className="sidebar__icons">
+      <li 
+        data-testid="inbox" 
+        className={active === 'inbox' ? 'active' : undefined }
+        onClick={() => {
+          setActive('inbox');
+          setSelectedProject('INBOX');
+        }}
+      >
+        <span>
           <FaInbox />
         </span>
         <span>Inbox</span>
       </li>
-      <li data-testid="today" className="today">
+      <li 
+        data-testid="today" 
+        className={active === "today" ? "active" : undefined }
+        onClick={() => {
+          setActive('today');
+          setSelectedProject('TODAY');
+        }}
+      >
         <span>
           <FaClipboardList />
         </span>
         <span>Today</span>
       </li>
-      <li data-testid="next_7" className="next_7">
+      <li 
+        data-testid="next_7" 
+        className={active === "next_7" ? "active" : undefined }
+        onClick={() => {
+          setActive('next_7');
+          setSelectedProject('NEXT_7');
+        }}
+       >
         <span>
           <FaCalendarWeek />
         </span>
@@ -25,14 +55,23 @@ export const Sidebar = () => (
       </li>
     </ul>
 
-    <div className="sidebar__middle">
-      <span><FaChevronCircleDown /></span>
+    <div className="sidebar__middle" onClick={() => setShowProjects(!showProjects)}>
+      <span>
+        <FaChevronCircleDown
+          className={!showProjects ? "hidden-projects" : undefined}
+       />
+       </span>
       <h2>Projects</h2>
     </div>
 
     <ul className="sidebar__projects">
-      Projects will be listed here
+      { showProjects && <Projects /> }
+      
     </ul>
-    Click to add new project
+    { showProjects && <AddProject /> }
   </div>
-);
+  );
+};
+
+
+
